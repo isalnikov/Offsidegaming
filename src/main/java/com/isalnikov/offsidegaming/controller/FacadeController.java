965751671a8de0765a93a563c1a6fd22b892fbc5
Igor Salnikov <igor.salnikov@stoloto.ru>
2020-12-17 14:36:43 +0300

    add pom banner log4j2 controller

--- src/main/java/com/isalnikov/offsidegaming/controller/FacadeController.java
+++ src/main/java/com/isalnikov/offsidegaming/controller/FacadeController.java
@@ -4,7 +4,6 @@ package com.isalnikov.offsidegaming.controller;
  *
  * @author isalnikov
  */
-
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,5 +12,11 @@ public class FacadeController {
     @RequestMapping("/")
     String home() {
         return "Hello World!";
+    }
+
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    public void disableFavicon() {
+
     }
 }
