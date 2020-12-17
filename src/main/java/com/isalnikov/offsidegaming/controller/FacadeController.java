3ebaef38f8e21a12a06352b1c6d71587e7265860
Igor Salnikov <igor.salnikov@stoloto.ru>
2020-12-17 14:16:32 +0300

    add pom banner log4j2 controller

--- src/main/java/com/isalnikov/offsidegaming/controller/FacadeController.java
+++ src/main/java/com/isalnikov/offsidegaming/controller/FacadeController.java
@@ -5,11 +5,9 @@ package com.isalnikov.offsidegaming.controller;
  * @author isalnikov
  */
 
-import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@EnableAutoConfiguration
 public class FacadeController {
 
     @RequestMapping("/")
